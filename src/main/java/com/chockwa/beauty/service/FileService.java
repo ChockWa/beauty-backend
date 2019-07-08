1a5413be1f29f7cd2cdd39583654e92bf53528b8
hezhuohua <hzh081215@qq.com>
2019-07-08 23:27:01 +0800

    生成描述文件

--- src/main/java/com/chockwa/beauty/service/FileService.java
+++ src/main/java/com/chockwa/beauty/service/FileService.java
@@ -229,7 +229,7 @@ public class FileService {
     }
 
     private File generateDescText(String targetFilePath) throws IOException {
-        File file = new File(targetFilePath + "24beauties.txt");
+        File file = new File(targetFilePath + "www.24beauties.xyz.txt");
         if(!file.exists()){
             file.createNewFile();
         }
