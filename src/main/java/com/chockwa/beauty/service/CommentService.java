7139fc3429e4136d4e733310a58cc27679e5c711
zhuohuahe <zhuohuahe@Ctrip.com>
2019-09-02 18:39:32 +0800

    完善接口

--- src/main/java/com/chockwa/beauty/service/CommentService.java
+++ src/main/java/com/chockwa/beauty/service/CommentService.java
@@ -1,5 +1,8 @@
 package com.chockwa.beauty.service;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chockwa.beauty.dto.PageParam;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * @auther: zhuohuahe
@@ -25,6 +29,14 @@ public class CommentService {
     private QmCommentMapper qmCommentMapper;
 
     public void qmComment(String qmId, String comment){
+        QueryWrapper<QmComment> query = new QueryWrapper<>();
+        query.lambda().eq(QmComment::getUid, UserInfo.get().getUid()).orderByDesc(QmComment::getCreateTime);
+        QmComment lastComment = qmCommentMapper.selectList(query).get(0);
+        if(Objects.nonNull(lastComment)){
+            if(DateUtil.between(new Date(), lastComment.getCreateTime(), DateUnit.MINUTE) < 3){
+                throw new IllegalStateException("兩次評論要相隔3分鐘，請稍後再試");
+            }
+        }
         QmComment qmComment = new QmComment();
         qmComment.setQmId(qmId);
         qmComment.setComment(comment);
